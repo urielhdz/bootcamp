09433d6c92c68c7e2ad4d91fe2c2f6511856ce24
urielhdz <uriel.hdzc@gmail.com>
2021-03-02 13:15:05 -0600

    Changes

--- projects/01-cipher/src/index.js
+++ projects/01-cipher/src/index.js
@@ -1,3 +1,23 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+
+const cipheredControl = document.querySelector('#translated-text');
+const plainTextControl = document.querySelector('#initial-text');
+const inputFactor = document.querySelector('#offset');
+const interchangeButton = document.querySelector('#interchange');
+
+function encodeUI(str) {
+  console.log(parseInt(inputFactor.value, 10), str);
+  cipheredControl.value = cipher.encode(parseInt(inputFactor.value, 10), str);
+}
+
+function decodeUI(str) {
+  plainTextControl.value = cipher.decode(parseInt(inputFactor.value, 10), str);
+}
+
+plainTextControl.addEventListener('change', () => encodeUI(plainTextControl.value));
+cipheredControl.addEventListener('change', () => decodeUI(cipheredControl.value));
+
+interchangeButton.addEventListener('click', () => {
+  document.querySelector("section").classList.toggle('reverse');
+});
