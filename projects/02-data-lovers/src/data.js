c09bd68d1a032834cade45699967fea1a4d707a2
urielhdz <uriel.hdzc@gmail.com>
2021-03-22 01:02:09 -0600

    Added test and functionality

--- projects/02-data-lovers/src/data.js
+++ projects/02-data-lovers/src/data.js
@@ -1,9 +1,24 @@
 // estas funciones son de ejemplo
-
-export const example = () => {
-  return 'example';
+// filterData([],{team: 'Mexico'})
+export const filterData = (data, conditions = {}) => {
+  let filteredData = data.athletes;
+  const filters = Object.entries(conditions);
+  filters.forEach(([condition, value]) => {
+    filteredData = filteredData.filter(athlete => athlete[condition] === value);
+  });
+  return { athletes: filteredData };
 };
 
-export const anotherExample = () => {
-  return 'OMG';
+export const sortData = (data, sortBy, sortOrder) => {
+  const sortedData = data.athletes.sort((firstEl, secondEl) => {
+    let result = firstEl[sortBy].localeCompare(secondEl[sortBy], undefined, { sensitivity: 'accent' });
+
+    if (sortOrder === 'desc') result *= -1;
+
+    return result;
+  });
+
+  return { athletes: sortedData };
 };
+
+export const demo = () => [];
--- projects/02-data-lovers/src/main.js
+++ projects/02-data-lovers/src/main.js
@@ -1,6 +1,74 @@
-import { example } from './data.js';
+import { filterData, sortData } from './data.js';
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
+// import data from './data/pokemon/pokemon.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, data);
+const buildUI = (data) => {
+  const root = document.querySelector('#athletes');
+  root.innerHTML = '';
+  const { athletes } = data;
+  athletes.forEach((athlete) => {
+    const node = document.createElement('article');
+    node.classList.add('athlete');
+    node.innerHTML = `
+      <div class='athlete__circle'>${athlete.noc}</div>
+      <header>
+        <h2>${athlete.name} - <span>${athlete.event}</span></h2>
+        <ul>
+          <li>${athlete.team}</li>
+          <li>${athlete.age}</li>
+          <li>${athlete.height}</li>
+          <li>${athlete.weight}</li>
+          <li>${athlete.gender}</li>
+        </ul>
+      </header>
+      <div class='athlete__medal'>
+        <img src='/icons/medals/${athlete.medal.toLowerCase()}.svg'  height='14' />
+        <p>${athlete.medal}</p>
+      </div>
+    `;
+    root.appendChild(node);
+  });
+};
+
+const setEvents = (data) => {
+  let stateData = data;
+  document.querySelector('.filters-button').addEventListener('click', () => {
+    document.querySelector('#filters').classList.toggle('open');
+  });
+
+  document.querySelector('.close-filters').addEventListener('click', () => {
+    document.querySelector('#filters').classList.remove('open');
+  });
+
+  document.querySelector('form#filters').addEventListener('submit', (ev) => {
+    ev.preventDefault();
+    const controls = document.querySelectorAll('form#filters input, form#filters select');
+    const filtersArr = Array.from(controls).map(control => [control.name, control.value]).filter(twoDimData => twoDimData[1] !== '');
+    const conditions = Object.fromEntries(filtersArr);
+    const filteredData = filterData(data, conditions);
+    stateData = filteredData;
+    buildUI(filteredData);
+    return false;
+  });
+
+  const sortUI = () => {
+    const sortOrder = document.querySelector('[name="sort-order"]').value;
+    const sortBy = document.querySelector('[name="sort"]').value;
+    const sortedData = sortData(stateData, sortBy, sortOrder);
+    stateData = sortedData;
+    buildUI(sortedData);
+  };
+
+  document.querySelector('[name="sort"]').addEventListener('change', sortUI);
+  document.querySelector('[name="sort-order"]').addEventListener('change', sortUI);
+};
+
+const init = async () => {
+  const response = await fetch('/data/athletes/athletes.json');
+  const data = await response.json();
+  buildUI(data);
+  setEvents(data);
+};
+
+init();
